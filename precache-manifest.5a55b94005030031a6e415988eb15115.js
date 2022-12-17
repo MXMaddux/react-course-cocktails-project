959d76126a581fd7eccea64847fd089dca774cdb
MXMaddux <maxmaddux@gmail.com>
2022-12-17 05:10:26 -0800

    first comit

---
+++ precache-manifest.5a55b94005030031a6e415988eb15115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50abf5333c6f70b2306a30501a6f2dfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b8d47ed70e67377f626",
+    "url": "/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "1bd42134c7c3757fe89a",
+    "url": "/static/js/2.63b8a9c3.chunk.js"
+  },
+  {
+    "revision": "12ef72f3e82558d66b92703a437e02e2",
+    "url": "/static/js/2.63b8a9c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b8d47ed70e67377f626",
+    "url": "/static/js/main.c566d1e7.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/static/media/logo.9a3d2645.svg"
+  }
+]);
